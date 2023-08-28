393612f422e2875c0a6b975c7bf6e1d136721cad
hgulese <hgulese02@gmail.com>
2023-08-29 00:39:33 +0300

    post.ejs edited.

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ app.get('/', async (req, res) => {
   });
 });
 
+app.get('/posts/:id', async (req, res) => {
+  // res.render('about');
+
+  const post = await Post.findById(req.params.id)
+  res.render('post', {
+    post
+  })
+});
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
